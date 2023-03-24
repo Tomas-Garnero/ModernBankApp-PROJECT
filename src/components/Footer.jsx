f8586c3235ddee2eeb814e28d863beb0dab6ec8e
unknown <tomygarnero9429@gmail.com>
2023-03-24 14:22:39 -0300

    Minimal Footer component changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,15 +41,14 @@ const Footer = () => (
             </p>
             <div className="flex flex-row md:mt-0 mt-6">
                 {socialMedia.map((social, index) => (
-                    <button type="button" title={social.title}>
                         <img
                         key={social.id}
                         src={social.icon}
                         alt={social.id}
                         className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`}
                         onClick={() => window.open(social.link)}
+                        title={social.title}
                         />
-                    </button>
                 ))}
             </div>
         </div>
