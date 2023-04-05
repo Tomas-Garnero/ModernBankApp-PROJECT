9cee98ca7fd0ca56923407f146288d3b79cfaf82
unknown <tomygarnero9429@gmail.com>
2023-04-05 11:19:44 -0300

    Some changes to Testimonial component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,17 +7,17 @@ const Testimonials = () => (
         <div className="absolute z-[0] w-[60%] h-[60%] -left-[30%] rounded-full blue__gradient top-0" />
         <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
         <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-            <h1 className={styles.heading2}>
+            <h1 className={`${styles.heading2} flex justify-center`}>
                 What People are <br className="sm:block hidden" /> saying about us
             </h1>
-            <div className="w-full md:mt-0 mt-6">
+            <div className="w-full md:mt-0 mt-6 flex justify-center">
                 <p className={`${styles.paragraph} text-left max-w-[450px]`}>
                     Everything you need to accept card payments and grow your business
                     anywhere on the planet.
                 </p>
             </div>
         </div>
-        <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        <div className="flex flex-wrap sm:justify-center justify-center w-full feedback-container relative z-[1]">
             {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
         </div>
     </section>
